d1ec1b7a1c4e5bf6ff319021ca5e02f17fa31642
Denis Golodkov <gold8523@gmail.com>
2016-10-28 16:12:51 +0300

    commit 4

--- dz1/index.php
+++ dz1/index.php
@@ -2,9 +2,8 @@
 // Задание 1
 $name ='Денис';
 $age = 26;
-echo 'Меня зовут: '. $name . PHP_EOL;
-echo PHP_EOL;
-echo "Мне $age лет" . PHP_EOL;
-echo '"!|\\/\'"\\' . PHP_EOL;
+echo 'Меня зовут: '. $name . "<br>";
+echo "Мне $age лет" . "<br>";
+echo '"!|\\/\'"\\' . "<br>";
 
 ?>
