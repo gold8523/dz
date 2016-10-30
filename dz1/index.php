32fd8dcc71b6f8b22c63a55c4ca50b5e36442cc6
Denis Golodkov <gold8523@gmail.com>
2016-10-30 21:15:31 +0300

    commit 25

--- dz1/index.php
+++ dz1/index.php
@@ -45,28 +45,18 @@ echo "<br>";
 echo "<br>";
 
 // Задание 5
-$day = 7;
+$day = 4;
 echo $day . "<br>";
 switch ($day) {
-    case (1);
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
         echo 'Это рабочий день';
         break;
-    case (2);
-        echo 'Это рабочий день';
-        break;
-    case (3);
-        echo 'Это рабочий день';
-        break;
-    case (4);
-        echo 'Это рабочий день';
-        break;
-    case(5);
-        echo 'Это рабочий день';
-        break;
-    case (6);
-        echo 'Это выходной день';
-        break;
-    case (7);
+    case 6:
+    case 7:
         echo 'Это выходной день';
         break;
     default;
