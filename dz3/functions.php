83561dd91be25c848d302b62ff9e4f721e6e1ddf
Denis Golodkov <gold8523@gmail.com>
2016-11-11 16:48:27 +0300

    commit 5

--- dz3/functions.php
+++ dz3/functions.php
@@ -1,19 +1,20 @@
 <?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=kursphp', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->exec('SET NAMES "utf-8"');
+$host = 'localhost';
+$base = 'phpkurs';
+$user = 'root';
+$pass = '';
 
+$connection = @new mysqli($host, $user, $pass, $base);
+if (mysqli_connect_errno()) {
+    die(mysqli_connect_error());
 }
-catch (PDOException $e) {
-    echo 'error';
-    exit();
-}
+$connection->query('SET NAMES "UTF-8"');
+
 $sql = "
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
   `age` int(3),
   PRIMARY KEY (`id`)
-) ";
-$pdo->query($sql);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
+$connection->query($sql);
