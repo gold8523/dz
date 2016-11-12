13d5261b5a1cf8aa159f2d17eedcd9fe35cb6caf
Denis Golodkov <gold8523@gmail.com>
2016-11-12 17:09:34 +0300

    commit 16

--- dz3/action1.php
+++ dz3/action1.php
@@ -9,16 +9,41 @@ if (mysqli_connect_errno()) {
     die(mysqli_connect_error());
 }
 $connection->query('SET NAMES "UTF-8"');
-if (!empty($_POST)) {
-    $sql = 'SELECT `user_id` FROM `login`';
+
+if (!empty($_POST['log'])) {
+    $sql = 'SELECT `user_id` FROM `login` ';
     $result = $connection->query($sql);
     $loginAll = $result->fetch_all(MYSQLI_ASSOC);
     foreach ($loginAll as $value) {
         foreach ($value as $item) {
-            $sql = 'SELECT `login`, `pass` FROM `login`';
-            $result = $connection->query($sql);
-            $loginAll = $result->fetch_all(MYSQLI_ASSOC);
+                $arrId [] = $item;
         }
     }
+    $sql = 'SELECT `login` FROM `login` ';
+    $result = $connection->query($sql);
+    $loginAll = $result->fetch_all(MYSQLI_ASSOC);
+    foreach ($loginAll as $value) {
+        foreach ($value as $item) {
+            $arrLogin [] = $item;
+        }
+    }
+    $sql = 'SELECT `pass` FROM `login` ';
+    $result = $connection->query($sql);
+    $loginAll = $result->fetch_all(MYSQLI_ASSOC);
+    foreach ($loginAll as $value) {
+        foreach ($value as $item) {
+            $arrPass [] = $item;
+        }
+    }
+    $len = count($arrId);
+    while ($len > -1) {
+        if ($arrLogin[$len] == strip_tags($_POST['log']) && $arrPass[$len] == strip_tags($_POST['password'])) {
+            header('Location: gallery.php');
+            exit();
+        }
+        $len--;
+    }
+    echo 'Неверный логин или пароль!';
+} else {
+    echo 'Введите логин и пароль!';
 }
-echo 'Неверный логин или пароль!';
