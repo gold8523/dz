a1dc96a1270202e39365cc45eebeeb7b11b6d3b2
Denis Golodkov <gold8523@gmail.com>
2016-11-14 22:50:01 +0300

    commit 29

--- dz3/gallery.php
+++ dz3/gallery.php
@@ -34,7 +34,7 @@ $lenMax = max(count($arrPic), count($arrId));
 </head>
 <body>
 <?php for ($i = 0; $i < $lenMax; $i++): ?>
-    <form action="functions.php" method="post">
+    <form action="action.php" method="post">
         <?php if (isset($arrPic[$i]) && isset($arrId[$i])) : ?>
             <label><?php echo $arrPic[$i]; ?>
                 <input type="hidden" name="id" value="<?php echo $arrId[$i]; ?>">
