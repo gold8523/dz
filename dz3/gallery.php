c38732e5f58444e96436db0771b21380f084c27b
Denis Golodkov <gold8523@gmail.com>
2016-11-16 14:56:08 +0300

    commit 34 Переписал вывод изображений из папки

--- dz3/gallery.php
+++ dz3/gallery.php
@@ -3,25 +3,16 @@ $host = 'localhost';
 $base = 'phpkurs';
 $user = 'root';
 $pass = '';
-
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $connection = @new mysqli($host, $user, $pass, $base);
 if (mysqli_connect_errno()) {
     die(mysqli_connect_error());
 }
-$connection->query('SET NAMES "UTF-8"');
 
-$sql = 'SELECT `img_id` FROM `images` ';
-$result = $connection->query($sql);
-$imgIdAll = $result->fetch_all(MYSQLI_ASSOC);
-foreach ($imgIdAll as $value) {
-    foreach ($value as $item) {
-        $arrId [] = $item;
-    }
-}
 $arrPic = scandir('photos');
 array_shift($arrPic);
 array_shift($arrPic);
-$lenMax = max(count($arrPic), count($arrPic));
+$len = count($arrPic);
 ?>
 <!doctype html>
 <html lang="en">
@@ -33,22 +24,26 @@ $lenMax = max(count($arrPic), count($arrPic));
     <title>Document</title>
 </head>
 <body>
-<?php for ($i = $lenMax; $i >= 0; $i--): ?>
+<?php $sqlImgId = 'SELECT `img_id` FROM `images` WHERE `img_name` = ?'; ?>
+<?php $stmt = $connection->prepare($sqlImgId); ?>
+<?php for ($i = 0; $i < $len; $i++): ?>
     <form action="action.php" method="post">
         <?php if (isset($arrPic[$i])) : ?>
-            <?php if (isset($arrId[$i])) : ?>
-                <label><?php echo $arrPic[$i]; ?>
-                    <input type="hidden" name="id" value="<?php echo $arrId[$i]; ?>">
-                    <input type="hidden" name="old" value="<?php echo $arrPic[$i]; ?>">
-                    <input type="text" name="edit" value="<?php echo $arrPic[$i]; ?>">
-                </label>
-                <input type="submit" name="action" value="Переименовать">
-                <input type="submit" name="action" value="Удалить"><br>
-            <?php endif; ?>
+            <?php $imgName = $arrPic[$i]; ?>
+            <?php $stmt->bind_param('s', $imgName); ?>
+            <?php $stmt->execute(); ?>
+            <?php $stmt->bind_result($imgName); ?>
+            <?php $id =  mysqli_stmt_fetch($stmt); ?>
+            <label><?php echo $arrPic[$i]; ?>
+                <input type="hidden" name="id" value="<?php echo $imgName; ?>">
+                <input type="hidden" name="old" value="<?php echo $arrPic[$i]; ?>">
+                <input type="text" name="edit" value="<?php echo $arrPic[$i]; ?>">
+            </label>
+            <input type="submit" name="action" value="Переименовать">
+            <input type="submit" name="action" value="Удалить"><br>
         <?php endif; ?>
     </form>
 <?php endfor; ?>
 <a href="index.php">Вернуться на главную</a>
 </body>
 </html>
-<!--isset($arrId[$i]))$arrPic[$i]-->
