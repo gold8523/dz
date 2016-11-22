9c897b34277bc73892b8d4d85cab047377228d27
Denis Golodkov <gold8523@gmail.com>
2016-11-22 18:49:15 +0300

    commit 9 авторизация

--- dz5/index.php
+++ dz5/index.php
@@ -1,6 +1,6 @@
 <?php
 include 'mainControl.php';
-include  'models\modLogin.php';
+include 'models\modLogin.php';
 
 $url = explode('/', $_SERVER['REQUEST_URI']);
 $cont = new controllers();
@@ -8,17 +8,26 @@ if (empty($url[2]) || $url[2] == 'index.php' || $url[2] == 'index.html') {
     include 'views/index.html';
 } else {
     $dir = explode('.', $url[2]);
-    switch ($dir[0]) {
-        case 'lk' :
-        case  'login' :
-        case 'form' :
-            include 'views/' . $dir[0] . '.html';
-            break;
-        default :
-            $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
-            header('HTTP/1.1 404 Not Found');
-            header('Status: 404 Not Found');
+    $contName = $url[2];
+    if ($dir[1] == 'html') {
+        $contName = 'views/' . $url[2];
+        $cont->control($contName);
+    } elseif ($dir[1] == 'php') {
+        $contName = 'controllers/' . $url[2];
+        $cont->control($contName);
+    } else {
+        switch ($dir[0]) {
+            case 'lk' :
+            case  'login' :
+            case 'form' :
+                include 'views/' . $dir[0] . '.html';
+                break;
+            default :
+                $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+                header('HTTP/1.1 404 Not Found');
+                header('Status: 404 Not Found');
 //            header('Location:'.$host.'404');
-            header('Location: 404.html');
+                header('Location: 404.html');
+        }
     }
 }
