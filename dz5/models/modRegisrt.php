2705e4dff8c509e4a7f4b76b819bfab2e940f7e9
Denis Golodkov <gold8523@gmail.com>
2016-11-22 19:39:56 +0300

    commit 11 начал регистрацию

--- dz5/models/modRegisrt.php
+++ dz5/models/modRegisrt.php
@@ -2,5 +2,34 @@
 include dirname(__DIR__) . '\mainModel.php';
 
 class modRegistr extends model {
+    public function registration {
+        $sqlUsers = 'insert into `users` (`username`, `age`, `info`) value (?, ?, ?)';
+        $sqlLogin = 'insert into `login` (`login`, `pass`) value (?, ?)';
+        $sqlImages = 'insert into `images` (`img_name`) value (?)';
 
+        $stmt = $connection->prepare($sqlUsers);
+
+        $username = strip_tags($_POST['name']);
+        $age = strip_tags($_POST['age']);
+        $info = strip_tags($_POST['info']);
+
+        $stmt->bind_param('sis', $username, $age, $info);
+        $stmt->execute();
+
+        $stmt = $connection->prepare($sqlLogin);
+
+        $login = strip_tags($_POST['login']);
+        $pass = strip_tags($_POST['pass']);
+
+        $stmt->bind_param('ss', $login, $pass);
+        $stmt->execute();
+
+        $stmt = $connection->prepare($sqlImages);
+
+        $name = strip_tags($_POST['login']) . '_' . $_FILES['picture']['name'];
+        $imgName = $name;
+
+        $stmt->bind_param('s', $imgName);
+        $stmt->execute();
+    }
 }
