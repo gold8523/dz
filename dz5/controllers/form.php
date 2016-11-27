a8fd08143db0e6cee56fc8acd1a58174193c8b98
Denis Golodkov <gold8523@gmail.com>
2016-11-27 14:55:18 +0300

    commit 24 PHP Mailer

--- dz5/controllers/form.php
+++ dz5/controllers/form.php
@@ -13,38 +13,41 @@ if (!empty($_POST) && $_POST['action'] == 'Зарегистрироваться'
     $imgNameCon = strip_tags($_POST['loginUser']) . '_' . $_FILES['picture']['name'];
 
 
+    $mail->SMTPDebug = 3;
+    $address = 'champ2013@yandex.ru';
+    $sub = 'Новый пользователь';
+    $body = 'Зарегистрирован новый пользователь на сайте!';
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'senior.phpovich2016@yandex.ru';                 // SMTP username
+    $mail->Password = '20pHp16';                           // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;
+
+    $mail->setFrom('senior.phpovich2016@yandex.ru');
+    $mail->addAddress($address, 'Петр');
+
+    $mail->Subject = $sub;
+    $mail->Body    = $body;
+    $mail->AltBody = $body;
+
+
     $insReg = $reg->registrationUs($usernameCon, $ageCon, $infoCon, $loginCon, $passCon, $imgNameCon);
 
 
     if (!empty($_FILES['picture']['name'])) {
 
-            if ($_FILES['picture']['type'] != "image/gif" && $_FILES['picture']['type'] != "image/jpeg"
-                && $_FILES['picture']['type'] != "image/png") {
-                echo  'Выберете изображение формата jpeg, png или gif.';
-            } else {
-                $dirUpload = dirname(__DIR__);
-                $uploads_dir = $dirUpload . '\uploads';
-                $tmp_name = $_FILES['picture']['tmp_name'];
-                move_uploaded_file($tmp_name, "$uploads_dir/$imgNameCon");
-            }
+        if ($_FILES['picture']['type'] != "image/gif" && $_FILES['picture']['type'] != "image/jpeg"
+            && $_FILES['picture']['type'] != "image/png"
+        ) {
+            echo 'Выберете изображение формата jpeg, png или gif.';
+        } else {
+            $dirUpload = dirname(__DIR__);
+            $uploads_dir = $dirUpload . '\uploads';
+            $tmp_name = $_FILES['picture']['tmp_name'];
+            move_uploaded_file($tmp_name, "$uploads_dir/$imgNameCon");
+        }
     }
-
-//    var_dump($_POST);
-//    $mail->SMTPDebug = 3;
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'senior.phpovich2016@yandex.ru';                 // SMTP username
-        $mail->Password = '20pHp16';                           // SMTP password
-        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 465;
-
-        $mail->setFrom('site.ru');
-        $mail->addAddress('senior.phpovich2016@yandex.ru', 'Петр');
-
-        $mail->Subject = 'Новый пользователь';
-        $mail->Body    = 'Зарегистрирован новый пользователь на сайте!';
-        $mail->AltBody = 'Зарегистрирован новый пользователь на сайте!';
-
-    header('Location: login.php');
+    header('Location: login.html');
 }
